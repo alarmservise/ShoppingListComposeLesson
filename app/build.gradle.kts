4edeb1fe1cd795a24f75af066af8e10aed9b595e
Alarm <alarmservise@mail.ru>
2023-09-09 11:51:31 +0300

    dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,19 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id ("kotlin-kapt")
+    //id ("kotlin-kapt")
+    id("com.google.devtools.ksp")
+    id ("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.alarmservise.shoppinglistcomposelesson"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.alarmservise.shoppinglistcomposelesson"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -31,12 +33,21 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
+   // compileOptions {
+   //     sourceCompatibility = JavaVersion.VERSION_1_8
+   //     targetCompatibility = JavaVersion.VERSION_1_8
+   // }
+   // kotlinOptions {
+    //    jvmTarget = "1.8"
+   // }
+
+
     buildFeatures {
         compose = true
     }
@@ -51,10 +62,26 @@ android {
 }
 
 dependencies {
+    //Data store
+    implementation("androidx.datastore:datastore-preferences-android:1.1.0-alpha05")
+    //Compose dependencies
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.navigation:navigation-compose:2.7.2")
+    //Dagger hilt
+    implementation("com.google.dagger:hilt-android:2.44.2")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    ksp("com.google.dagger:hilt-compiler:2.44.2")
+    ksp("androidx.hilt:hilt-compiler:1.0.0")
+    //Room
+    implementation("androidx.room:room-runtime:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
