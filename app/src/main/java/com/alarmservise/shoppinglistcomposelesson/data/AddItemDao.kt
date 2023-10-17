3fe19c3218e6b094ef9efcda018f12364a49f741
Alarm <alarmservise@mail.ru>
2023-10-17 21:11:03 +0300

    lesson4 @Dao

---
+++ app/src/main/java/com/alarmservise/shoppinglistcomposelesson/data/AddItemDao.kt
@@ -0,0 +1,22 @@
+package com.alarmservise.shoppinglistcomposelesson.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AddItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem (item: AddItem)
+    @Delete
+    suspend fun deleteItem (item: AddItem)
+    @Query("SELECT * FROM add_item WHERE listId = :listId ")
+    fun getAllItemsById(listId: Int): Flow<List<AddItem>>
+    @Query ("SELECT * FROM shop_list_name WHERE id = :listId")
+    suspend fun getListItemsById(listId: Int): ShoppingListItem
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem (item: ShoppingListItem)
+}
